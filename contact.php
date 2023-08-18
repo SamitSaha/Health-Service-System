50d6e546afc3c7bbfb6acd8b38261cd272f2599d
SamitSaha <72096509+SamitSaha@users.noreply.github.com>
2023-08-18 21:39:52 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+    $connection = mysqli_connect('localhost', 'root', '','health_contact');
+
+    if(isset($_POST['send'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $text = $_POST['text'];
+        
+
+        $request = " insert into health_contact(name,email,phone,text) values
+        ('$name','$email','$phone','$text') ";
+        mysqli_query($connection, $request);
+        header('location:Contact.html');
+
+    }
+    else{
+        echo 'something went wrong try again';
+        
+    }
+?>
