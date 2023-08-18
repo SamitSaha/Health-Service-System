a4a20d9074c50bfb3a1d75b26a7bad14a64b34de
SamitSaha <72096509+SamitSaha@users.noreply.github.com>
2023-08-18 16:25:52 +0600

    Update welcome.php

--- LOGIN/welcome.php
+++ LOGIN/welcome.php
@@ -35,7 +35,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="register.php">Register</a>
